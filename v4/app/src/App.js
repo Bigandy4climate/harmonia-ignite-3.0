eaca22cdd11b9841fdf5e5a47adc33de0edc98e8
nate <natnael.getenew@addissoftware.com>
2021-09-20 23:32:47 +0300

    trying to add router

--- v4/app/src/App.js
+++ v4/app/src/App.js
@@ -7,6 +7,8 @@ import {
   ConnectionProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Registry from "./Registry";
 
 const wallets = [getPhantomWallet()];
 const network = clusterApiUrl("devnet");
@@ -14,9 +16,54 @@ const network = clusterApiUrl("devnet");
 function App() {
   return (
     <div className="App">
+      <h1>hello</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/Registry">Registry</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/Registry">
+              <Registry />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
       <Album></Album>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 const AppWithProvider = () => (
